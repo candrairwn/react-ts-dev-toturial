18eb6c98379a6dcd094c9b0b2913ed7d2d46eee6
candrairwn <candrai1726@gmail.com>
2023-11-01 10:15:57 +0700

    remove: unneceserry

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,6 @@ import { PRODUCTS } from './data/products/products'
 import FilterableProducts from '@pages/FilterableProducts'
 
 function App() {
-  const { numberAlias }: { numberAlias?: string } = {}
-
-  const a = numberAlias
-  console.info(a)
-
   return <FilterableProducts products={PRODUCTS} />
 }
 
